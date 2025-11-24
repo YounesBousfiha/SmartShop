882fccc480134ffdb305b5682050cea48dad1583
Coderyounes <younesanas2021@gmail.com>
2025-11-24 16:27:21 +0100

    FEAT(SMR-18): add OrderItem entity with attributes and JPA annotations

---
+++ src/main/java/com/jartiste/smartshop/domain/entity/OrderItem.java
@@ -0,0 +1,36 @@
+package com.jartiste.smartshop.domain.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    private BigDecimal unitPrice;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
