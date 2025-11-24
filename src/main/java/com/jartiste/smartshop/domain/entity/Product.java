dc7c72af8732d397d58ab5f47c216f31fed53a49
Coderyounes <younesanas2021@gmail.com>
2025-11-24 16:26:10 +0100

    FEAT(SMR-18): add Product entity with attributes and JPA annotations

---
+++ src/main/java/com/jartiste/smartshop/domain/entity/Product.java
@@ -0,0 +1,37 @@
+package com.jartiste.smartshop.domain.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private Integer stock;
+
+    private Boolean deleted;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
