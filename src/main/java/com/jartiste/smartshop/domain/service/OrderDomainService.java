40f805c41a27f55a81e164203ff17fd62a5bd616
Coderyounes <younesanas2021@gmail.com>
2025-11-25 15:59:50 +0100

    FEAT: implement OrderDomainService for order initialization and processing

---
+++ src/main/java/com/jartiste/smartshop/domain/service/OrderDomainService.java
@@ -0,0 +1,61 @@
+package com.jartiste.smartshop.domain.service;
+
+import com.jartiste.smartshop.domain.entity.Client;
+import com.jartiste.smartshop.domain.entity.Order;
+import com.jartiste.smartshop.domain.entity.OrderItem;
+import com.jartiste.smartshop.domain.enums.OrderStatus;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+
+@Service
+public class OrderDomainService {
+
+    public Order initializeOrder(Client client, String promoCode) {
+        return Order.builder()
+                .client(client)
+                .promoCode(promoCode)
+                .orderStatus(OrderStatus.PENDING)
+                .build();
+    }
+
+    public void processOrderItem(Order order, List<OrderItem> itemList) {
+        BigDecimal subTotal = BigDecimal.ZERO;
+
+        for(OrderItem item: itemList) {
+            item.getProduct().decreaseStock(item.getQuantity());
+
+            item.setOrder(order);
+            order.getItemList().add(item);
+
+            BigDecimal linetotal = item.getUnitPrice().multiply(BigDecimal.valueOf(item.getQuantity()));
+            subTotal = subTotal.add(linetotal);
+        }
+
+        order.setSubTotal(subTotal);
+    }
+
+    public void calculateFinalAmounts(Order order) {
+
+        BigDecimal discountRate = order.getClient().getDiscountRate(order.getSubTotal());
+        BigDecimal discountAmount = order.getSubTotal().multiply(discountRate);
+
+        if(order.getPromoCode() != null && order.getPromoCode().matches("PROMO-[A-Z0-9]{4}")) {
+            discountAmount = discountRate.add(order.getSubTotal().multiply(BigDecimal.valueOf(0.05)));
+        }
+
+        order.setDiscountAmount(discountAmount.setScale(2, RoundingMode.HALF_UP));
+
+        BigDecimal netHT = order.getSubTotal().subtract(order.getDiscountAmount());
+        if(netHT.compareTo(BigDecimal.ZERO) < 0) netHT = BigDecimal.ZERO;
+
+        BigDecimal tva = netHT.multiply(BigDecimal.valueOf(0.20));
+        order.setTaxAmount(tva.setScale(2, RoundingMode.HALF_UP));
+
+        BigDecimal total = netHT.add(tva).setScale(2, RoundingMode.HALF_UP);
+        order.setTotalAmount(total);
+        order.setRemainingAmount(total);
+    }
+}
