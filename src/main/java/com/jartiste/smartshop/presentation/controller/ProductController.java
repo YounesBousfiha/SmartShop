4664556f133d399eba72d75a31c1c8a66287fe58
Coderyounes <younesanas2021@gmail.com>
2025-11-25 14:09:07 +0100

    FEAT(SMR-29): add ProductController for managing product operations

---
+++ src/main/java/com/jartiste/smartshop/presentation/controller/ProductController.java
@@ -0,0 +1,65 @@
+package com.jartiste.smartshop.presentation.controller;
+
+import com.jartiste.smartshop.application.service.ProductService;
+import com.jartiste.smartshop.domain.enums.UserRole;
+import com.jartiste.smartshop.presentation.annotation.RequireRole;
+import com.jartiste.smartshop.presentation.dto.request.ProductRequest;
+import com.jartiste.smartshop.presentation.dto.response.ProductResponse;
+import jakarta.persistence.PreUpdate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    @RequireRole({UserRole.ADMIN, UserRole.CLIENT})
+    public ResponseEntity<Page<ProductResponse>> getAllProducts(int page, int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<ProductResponse> responses = this.productService.getAllActiveProduct(pageable);
+
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/{id}")
+    @RequireRole({UserRole.ADMIN, UserRole.CLIENT})
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable Long id) {
+        ProductResponse response = this.productService.getProductById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    @RequireRole(UserRole.ADMIN)
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest request) {
+        ProductResponse response = this.productService.createProduct(request);
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    @RequireRole(UserRole.ADMIN)
+    public ResponseEntity<ProductResponse> updateProduct(
+            @PathVariable Long id,
+            @RequestBody ProductRequest request
+    ) {
+        ProductResponse response = this.productService.updateProduct(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    @RequireRole(UserRole.ADMIN)
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        this.productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+}
