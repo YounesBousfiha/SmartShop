0b7737ecadc0e94e73cd2c167dd54e2ea2356393
Coderyounes <younesanas2021@gmail.com>
2025-11-26 15:07:03 +0100

    FEAT: add unit tests for OrderDomainService to validate order initialization and stock management

---
+++ src/test/java/com/jartiste/smartshop/domain/service/OrderDomainServiceTest.java
@@ -0,0 +1,128 @@
+package com.jartiste.smartshop.domain.service;
+
+
+import com.jartiste.smartshop.domain.entity.Client;
+import com.jartiste.smartshop.domain.entity.Order;
+import com.jartiste.smartshop.domain.entity.OrderItem;
+import com.jartiste.smartshop.domain.entity.Product;
+import com.jartiste.smartshop.domain.enums.CustomerTier;
+import com.jartiste.smartshop.domain.enums.OrderStatus;
+import com.jartiste.smartshop.domain.exception.BusinessLogicViolation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class OrderDomainServiceTest {
+
+    @InjectMocks
+    private OrderDomainService orderDomainService;
+
+    private Client goldClient;
+    private Product laptop;
+
+    @BeforeEach
+    void setup() {
+        goldClient = Client.builder()
+                .id(1L)
+                .username("gold_user")
+                .tier(CustomerTier.GOLD)
+                .build();
+
+        laptop = Product.builder()
+                .id(100L)
+                .name("MacBook")
+                .price(BigDecimal.valueOf(1000.00))
+                .stock(10)
+                .deleted(false)
+                .build();
+    }
+
+    @Test
+    @DisplayName("Should Set the Data")
+    void initializeOrderTest() {
+        // public Order initializeOrder(Client client, String promoCode)
+
+        Order order = orderDomainService.initializeOrder(goldClient, "PROMO-1234");
+
+        assertEquals(goldClient.getNom(), order.getClient().getNom());
+        assertEquals("PROMO-1234", order.getPromoCode());
+        assertEquals(OrderStatus.PENDING, order.getOrderStatus());
+    }
+
+    @Test
+    @DisplayName("Should Calculate correct total for GOLD Client")
+    void shouldCalculateTotalWithGoldDiscount() {
+        Order order = Order.builder()
+                .client(goldClient)
+                .subTotal(BigDecimal.valueOf(1000.00))
+                .build();
+
+        orderDomainService.calculateFinalAmounts(order);
+
+        assertEquals(0, BigDecimal.valueOf(1000.00).compareTo(order.getSubTotal()));
+        assertEquals(0, BigDecimal.valueOf(180.00).compareTo(order.getTaxAmount()));
+        assertEquals(0, BigDecimal.valueOf(1080.00).compareTo(order.getTotalAmount()));
+    }
+
+    @Test
+    @DisplayName("Should calculate total for Basic Client without discount")
+    void shouldCalculateTotalForBasicClient() {
+        Client clietBasic = Client.builder()
+                .tier(CustomerTier.BASIC)
+                .build();
+
+        Order order = Order.builder()
+                .client(clietBasic)
+                .subTotal(BigDecimal.valueOf(1000.00))
+                .build();
+
+
+        orderDomainService.calculateFinalAmounts(order);
+
+        assertEquals(0, BigDecimal.ZERO.compareTo(order.getDiscountAmount()));
+        assertEquals(0, BigDecimal.valueOf(200.00).compareTo(order.getTaxAmount()));
+    }
+
+    @Test
+    @DisplayName("Should decrease product stock when processing it")
+    void shouldDecreseStock() {
+        Order order = new Order();
+        order.setItemList(new ArrayList<>());
+
+        OrderItem item = OrderItem.builder()
+                .product(laptop)
+                .quantity(2)
+                .unitPrice(laptop.getPrice())
+                .build();
+
+        orderDomainService.processOrderItem(order, List.of(item));
+
+        assertEquals(8, laptop.getStock());
+        assertEquals(BigDecimal.valueOf(2000.00), order.getSubTotal());
+    }
+
+    @Test
+    @DisplayName("Should Throw expcetion when stock is insufficient")
+    void ShouldThrowExceptionWhenStockIsLow() {
+        Order order = new Order();
+        OrderItem item = OrderItem.builder()
+                .product(laptop)
+                .quantity(20)
+                .build();
+
+        assertThrows(BusinessLogicViolation.class, () -> {
+            orderDomainService.processOrderItem(order, List.of(item));
+        });
+    }
+}
