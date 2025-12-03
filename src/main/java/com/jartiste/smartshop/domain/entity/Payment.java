e04673bb1b17d51e68048c647de49a9a68b63b41
Coderyounes <younesanas2021@gmail.com>
2025-12-04 00:55:01 +0100

    FEAT: remove unused import from Payment class for cleaner code

--- src/main/java/com/jartiste/smartshop/domain/entity/Payment.java
+++ src/main/java/com/jartiste/smartshop/domain/entity/Payment.java
@@ -7,7 +7,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.cglib.core.Local;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
